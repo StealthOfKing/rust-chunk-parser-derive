790c783bc78081b2279cefc6f27c1144689cf933
SoK <sok@monocyte.host>
2024-10-03 03:27:22 +0100

    added support for custom struct fields

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,51 @@
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Data, Fields, Field};
 
 #[proc_macro_attribute]
 pub fn chunk_parser(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput); // parse syntax tree
+
     let name = input.ident; // extract struct name
+    let mut fields = quote! {}; // struct fields
+    let mut defaults = quote! {}; // default initialisers
+
+    // generate existing fields
+    if let Data::Struct(data) = input.data {
+        if let Fields::Named(existing) = data.fields {
+            for field in existing.named {
+                let Field { ident, ty, .. } = field;
+                fields.extend(quote! { #ident: #ty, });
+                defaults.extend(quote! { #ident: #ty::default() });
+            }
+        }
+    }
+
+    // append internal parser fields
+    fields.extend(quote! {
+        reader: R,
+        depth: usize,
+    });
 
     // generate expanded struct definition
     return quote! {
 
         // the internal reader forms the backbone of the parser
         pub struct #name<R> where R: std::io::Read + std::io::Seek {
-            reader: R,
-            depth: usize
+            #fields
         }
 
         // general ctor
         impl<R> #name<R> where R: std::io::Read + std::io::Seek {
-            #[inline] pub fn new(reader: R) -> Self { Self { reader, depth: 0 } }
+            #[inline] pub fn new(reader: R) -> Self
+                { Self { reader, depth: 0, #defaults } }
         }
 
         // buffer ctor
         impl #name<std::io::Cursor<&[u8]>> {
-            #[inline] pub fn buf(buffer: &'static [u8]) -> Self {
-                Self::new(std::io::Cursor::new(buffer))
-            }
+            #[inline] pub fn buf(buffer: &'static [u8]) -> Self
+                { Self::new(std::io::Cursor::new(buffer)) }
         }
 
         // file ctor
