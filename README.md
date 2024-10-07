db1e898b39c51b9f53fcaa426349be01833ff6d6
SoK <sok@monocyte.host>
2024-10-07 07:28:16 +0100

    refactored chunk_parser

--- README.md
+++ README.md
@@ -13,12 +13,54 @@ The macro implements everything required to define a specialised chunk parser:
 ```rust
 use chunk_parser::prelude::*;
 
+// Decorate IFFParser with chunk parser API.
 #[chunk_parser]
 struct IFFParser;
-impl<R> Parser for IFFParser<R> where R: std::io::Read + std::io::Seek {
-    type Header = (TypeId, i32);
-    fn parse_header(&mut self) -> Result<Self::Header> {
-        Ok((self.read()?, self.read_be()?))
+
+// Define the chunk header layout.
+struct IFFHeader { typeid: TypeId, length: u32 }
+
+// Define the chunk header parsing logic.
+impl<R: Read> HeaderParser<IFFHeader> for IFFParser<R> {
+    fn header(&mut self) -> Result<IFFHeader>
+        { Ok( IFFHeader { typeid: self.read()?, length: self.read_be()? } ) }
+}
+```
+
+## Custom
+
+To define a custom parser loop, pass `custom` to the attribute macro and define a `ChunkParser` implementation.
+Optionally include the `depth` attribute to add `depth()`, `pop()` and `parse()`.
+
+```rust
+#[chunk_parser(custom,depth)]
+struct IFFParserCustom {}
+
+// Optionally define a header in order to use parser.header().
+impl<R: Read> HeaderParser<IFFHeader> for IFFParserCustom<R> {
+    fn header(&mut self) -> Result<IFFHeader>
+        { Ok( IFFHeader { typeid: self.read()?, length: self.read_be::<u32>()? - 8 } ) }
+}
+
+// Define a custom chunk parser loop.
+impl<R: std::io::Read + std::io::Seek> ChunkParser<R> for IFFParserCustom<R> {
+    fn parse_loop<H>(&mut self, f: ParserFn<Self,H>, total_size: u64) -> Result<()> where Self: HeaderParser<H> {
+        self.push();
+        match loop {
+            let header = self.header()?;
+            let start = self.reader().stream_position()?;
+            let size = f(self, &header)? + 8; // the parser function is responsible for parsing the size
+            let end = start + size;
+            let pos = self.reader().stream_position()?;
+            if pos == total_size { break Ok(()) } // function consumed chunk
+            else if pos != end { break Err(Error::ParseError) } // function made a mistake
+        } {
+            res => { self.pop(); res }
+        }
     }
 }
+
+// Use the custom parser with adjusted length.
+let mut iff = IFFParserCustom::cursor(DATA);
+iff.parse(|parser, header| parser.skip(header.length as u64 + 8))
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,20 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Data, Fields, Field};
 
+// this macro generates required and optional implementations for ChunkParser
 #[proc_macro_attribute]
-pub fn chunk_parser(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn chunk_parser(args: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput); // parse syntax tree
 
     let name = input.ident; // extract struct name
     let mut fields = quote! {}; // struct fields
     let mut defaults = quote! {}; // default initialisers
+    let mut impls = quote! {}; // implementations
+
+    // optional feature config
+    let mut has_depth = false;
+    let mut is_custom = false;
+    let mut needs_depth = false;
 
     // generate existing fields
     if let Data::Struct(data) = input.data {
@@ -19,33 +26,66 @@ pub fn chunk_parser(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 let Field { ident, ty, .. } = field;
                 fields.extend(quote! { #ident: #ty, });
                 defaults.extend(quote! { #ident: #ty::default() });
+                if ident.unwrap().to_string() == "depth" {
+                    has_depth = true;
+                }
             }
         }
     }
 
-    // append internal parser fields
-    fields.extend(quote! {
-        reader: R,
-        depth: isize,
+    // append inner parser fields
+    fields.extend(quote! { reader: R, });
+
+    // process extra attribute features
+    let parser = syn::meta::parser(|meta| {
+        if !has_depth && meta.path.is_ident("depth") {
+            needs_depth = true;
+            Ok(())
+        } else if meta.path.is_ident("custom") {
+            is_custom = true;
+            Ok(())
+        } else {
+            Err(meta.error("unsupported attribute"))
+        }
     });
+    parse_macro_input!(args with parser);
+
+    // depth attribute feature
+    if needs_depth || !is_custom {
+        fields.extend(quote! { depth: u8, });
+        defaults.extend(quote! { depth: 0, });
+        impls.extend(quote! {
+            impl<R> ParserDepth for #name<R> {
+                #[inline] fn inner_depth(&mut self) -> &mut u8
+                    { &mut self.depth }
+            }
+        });
+    }
+
+    // default parser loop attribute feature
+    if !is_custom {
+        impls.extend(quote! {
+            impl<R: std::io::Read + std::io::Seek> ChunkParser<R> for #name<R> {}
+        });
+    }
 
     // generate expanded struct definition
     return quote! {
 
-        // the internal reader forms the backbone of the parser
-        pub struct #name<R> where R: std::io::Read + std::io::Seek {
+        // the inner reader forms the backbone of the parser
+        pub struct #name<R> {
             #fields
         }
 
         // general ctor
-        impl<R> #name<R> where R: std::io::Read + std::io::Seek {
+        impl<R: std::io::Read> #name<R> {
             #[inline] pub fn new(reader: R) -> Self
-                { Self { reader, depth: 0, #defaults } }
+                { Self { reader, #defaults } }
         }
 
         // buffer ctor
         impl #name<std::io::Cursor<&[u8]>> {
-            #[inline] pub fn buf(buffer: &'static [u8]) -> Self
+            #[inline] pub fn cursor(buffer: &'static [u8]) -> Self
                 { Self::new(std::io::Cursor::new(buffer)) }
         }
 
@@ -58,13 +98,14 @@ pub fn chunk_parser(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
 
         // implement parser interface
-        impl<R> ParserInner for #name<R> where R: std::io::Read + std::io::Seek {
-            type Reader = R;
-            #[inline] fn reader(&mut self) -> &mut Self::Reader { &mut self.reader }
-            #[inline] fn depth(&self) -> usize { self.depth as usize }
-            #[inline] fn push(&mut self) { self.depth += 1; }
-            #[inline] fn pop(&mut self) { self.depth -= 1; }
+        impl<R> ParserReader<R> for #name<R> {
+            #[inline] fn reader(&mut self) -> &mut R
+                { &mut self.reader }
         }
+        impl<R: std::io::Seek> ParserSeek<R> for #name<R> {}
+        impl<R: std::io::Read> ParserRead<R> for #name<R> {}
+
+        #impls
 
     }.into()
 }
